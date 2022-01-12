c458c6a9376ae2e0341bc1ab1e1c3e54d13c0d5a
Welton Rodrigo <weltonjc1@hotmail.com>
2022-01-12 16:02:27 -0300

    UPDATE para categoria

--- src/main/java/com/welton/bookstore/service/CategoriaService.java
+++ src/main/java/com/welton/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.welton.bookstore.domain.Categoria;
+import com.welton.bookstore.dtos.CategoriaDTO;
 import com.welton.bookstore.repositories.CategoriaRepository;
 
 @Service
@@ -28,6 +29,14 @@ public class CategoriaService {
 	
 	public Categoria create(Categoria obj) {
 		obj.setId(null);
+		return repository.save(obj);
+	}
+
+	public Categoria update(Integer id, CategoriaDTO objDto) {
+		// TODO Auto-generated method stub
+		Categoria obj = findById(id);
+		obj.setNome(objDto.getNome());
+		obj.setDescricao(objDto.getDescricao());
 		return repository.save(obj);
 	}
 	
